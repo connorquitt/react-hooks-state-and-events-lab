215b09bc96f2cba37f649e7a374551baa6d4298c
connorq <connorquitt@gmail.com>
2023-10-28 15:42:51 -0500

    all tests passing

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isAdd, addToCart] = useState(true)
+
+  const cart = isAdd ? 'Add to Cart' : 'Remove from Cart'
+  const itemClass = isAdd ? '' : 'in-cart'
+
+  function handleClick(){
+    addToCart(!isAdd)
+  }
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>{cart}</button>
     </li>
   );
 }
